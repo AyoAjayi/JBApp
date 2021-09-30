441e977938a03e9df473ad5c3600c2e584ed90b7
Ayo Ajayi <ayomide.ajayi2839@gmail.com>
2021-09-29 21:04:40 -0400

    Completed Task 1

--- hw4_starter/src/test/java/EmployerTest.java
+++ hw4_starter/src/test/java/EmployerTest.java
@@ -25,6 +25,7 @@ public class EmployerTest {
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     class EmployerORMLiteDaoTest {
 
+
         private ConnectionSource connectionSource;
         private Dao<Employer, Integer> dao;
 
@@ -44,8 +45,8 @@ public class EmployerTest {
             TableUtils.clearTable(connectionSource, Employer.class);
         }
 
-        // inserting a new record where name is null must fail, the reason being
-        // there is a non-null constraint on the "name" column in "employers" table!
+        //         inserting a new record where name is null must fail, the reason being
+//         there is a non-null constraint on the "name" column in "employers" table!
         @Test
         public void testCreateNameNull() {
             //create a new employer instance
@@ -54,19 +55,20 @@ public class EmployerTest {
             Assertions.assertThrows(SQLException.class, () -> dao.create(e));
         }
 
-        // inserting a new record where sector is an empty string must succeed!
+        //inserting a new record where sector is an empty string must succeed!
         @Test
         public void testCreateSectorEmpty() throws SQLException {
             // create a new employer instance
             Employer e = new Employer("Company1", "", "Summary");
             // try to insert into employers table. This must succeed!
             dao.create(e);
+
             List<Employer> ls = dao.queryForEq("name", e.getName());
             assertEquals(ls.size(), 1);
             assertEquals("", ls.get(0).getSector());
         }
-
-        // insert multiple employer records, and assert they were indeed added!
+        //
+//        // insert multiple employer records, and assert they were indeed added!
         @Test
         public void testReadMutipleEmployers() throws SQLException {
             // create multiple new employer instance
@@ -83,7 +85,7 @@ public class EmployerTest {
             assertEquals(lsCreate, lsRead);
         }
 
-        // insert a new record, then delete it, and assert it was indeed removed!
+        //        // insert a new record, then delete it, and assert it was indeed removed!
         @Test
         public void testDeleteAllFieldsMatch() throws SQLException {
             // create a new employer instance
@@ -113,25 +115,135 @@ public class EmployerTest {
             assertEquals("Food/Beverage", dao.queryForEq("name", "Kraft Heinz").get(0).getSector());
         }
 
+
         // TODO 1: Think of more test cases for all CRUD operations and add them below!
         //  Write a test case for each of the following scenarios and assert the expected outcome:
         //  C(reate):
-        //      1) inserting two employers with same exact name fails
-        //      2) inserting two employers with same exact sector succeeds
-        //      3) inserting an employer with an empty name succeeds
-        //      4) inserting an employer where employer's id is set manually on the
-        //         employer object (using setId(int) method) is not (necessarily) the id value
-        //         that gets inserted in the table!
-        //  U(pdate)
-        //      1) updating an employer's id to an id value that does NOT exist in the table succeeds
-        //      2) updating an employer's id to an id value that exists in the table fails
-        //      3) updating an employer's name that is already exists in the table succeeds
-        //      4) updating an employer's summary that already exists in the table succeeds
+
+        // 1) inserting two employers with same exact name fails
+        @Test
+        public void testSameEmployerNames() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            myList.add(new Employer("Ayomide", "Biochemistry", "An American cloud-based software company focused on customer relationship management services!"));
+            myList.add(new Employer("Ayomide", "Tech", "Sonos is a developer and manufacturer of audio products best known for its multi-room audio products!"));
+            Assertions.assertThrows(SQLException.class, () -> dao.create(myList));
+        }
+
+        //  2) inserting two employers with same exact sector succeeds
+        @Test
+        public void testSameEmployerSector() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            myList.add(new Employer("Ayomide", "Tech", "An American cloud-based software company focused on customer relationship management services!"));
+            myList.add(new Employer("James", "Tech", "Sonos is a developer and manufacturer of audio products best known for its multi-room audio products!"));
+            Assertions.assertDoesNotThrow(() -> dao.create(myList));
+        }
+
+        // 3) inserting an employer with an empty name succeeds
+        @Test
+        public void testEmptyEmployerName() throws SQLException {
+            Employer e = new Employer("", "Tech", "An American cloud-based software company focused on customer relationship management services!");
+            dao.create(e);
+            Assertions.assertEquals("", e.getName());
+        }
+
+        // 4) inserting an employer where employer's id is set manually on the employer object (using setId(int) method) is not (necessarily) the id value that gets inserted in the table!
+        @Test
+        public void testManuallySetEmployerID() throws SQLException {
+            Employer e = new Employer("", "Dance", "An American cloud-based software company focused on customer relationship management services!");
+            e.setId(5);
+            dao.create(e);
+            Assertions.assertNotEquals(5, e.getId());
+        }
+
+        //U(pdate)
+        // 1) updating an employer's id to an id value that does NOT exist in the table succeeds
+        @Test
+        public void testUpdateEmployerIDSucceed() throws SQLException {
+            Employer e = new Employer("Ebuka", "Singing", "An American cloud-based software company focused on customer relationship management services!");
+            dao.create(e);
+            dao.updateId(e,15);
+            assertEquals(15, e.getId());
+        }
+
+        //2) updating an employer's id to an id value that exists in the table fails
+        @Test
+        public void testUpdateEmployerIDFail() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            myList.add(new Employer("John", "Tech", "An American cloud-based software company focused on customer relationship management services!"));
+            myList.add(new Employer("Jack", "Tech", "Sonos is a developer and manufacturer of audio products best known for its multi-room audio products!"));
+            dao.create(myList);
+            Assertions.assertThrows(SQLException.class, () ->  dao.updateId(myList.get(0),myList.get(1).getId()));
+        }
+
+        //3) updating an employer's name that already exists in the table succeeds
+        @Test
+        public void testUpdateEmployerNameSuccess() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            myList.add(new Employer("John", "Tech", "An American cloud-based software company focused on customer relationship management services!"));
+            myList.add(new Employer("Jack", "Tech", "Sonos is a developer and manufacturer of audio products best known for its multi-room audio products!"));
+            dao.create(myList);
+            myList.get(0).setName("Jason");
+            Assertions.assertDoesNotThrow(() -> dao.update(myList.get(0)), "No Error");
+        }
+
+        // 4) updating an employer's summary that already exists in the table succeeds
+        @Test
+        public void testUpdateEmployerSummarySuccess() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            myList.add(new Employer("John", "Tech", "An American cloud-based software company focused on customer relationship management services!"));
+            myList.add(new Employer("Jack", "Tech", "Sonos is a developer and manufacturer of audio products best known for its multi-room audio products!"));
+            dao.create(myList);
+            myList.get(1).setSummary("This is the new summary");
+            Assertions.assertDoesNotThrow(() ->  dao.update(myList.get(1)), "No Error");
+        }
+
         //  D(elete)
-        //      1) Deleting an employer record (using the "delete" function of ORMLite) based on an id that does not exist does not delete any rows even if a row with the same exact name exists
-        //      2) Deleting an employer record (using the "delete" function of ORMLite) based on an id that exists succeeds even if the names are different
-        //      3) Deleting a collection of employers at once (using the "delete" function of ORMLite) removes all those employers that exist from the table
-        //      4) Deleting a collection of employers at once (using the "delete" function of ORMLite) where none of them exists does not remove any rows from the table
+        //  1) Deleting an employer record
+        //  (using the "delete" function of ORMLite) based on an id that does not exist does not delete any rows even if
+        //  a row with the same exact name exists
+        @Test
+        public void testDeleteEmployerRecordFails() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            myList.add(new Employer("John", "Tech", "An American cloud-based software company focused on customer relationship management services!"));
+            myList.add(new Employer("Jon", "Tech", "Sonos is a developer and manufacturer of audio products best known for its multi-room audio products!"));
+            dao.create(myList);
+            dao.updateId(myList.get(0),100);
+            dao.delete(dao.queryForId(10));
+            assertEquals(myList.size(),dao.queryForAll().size());
+
+            Assertions.assertDoesNotThrow(() ->  dao.delete(dao.queryForId(10)), "Test passed");
+        }
+
+        // 2) Deleting an employer record (using the "delete" function of ORMLite) based on an id that exists succeeds even if the names are different
+        @Test
+        public void testDeleteEmployerRecordSucceeds() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            myList.add(new Employer("John", "Tech", "An American cloud-based software company focused on customer relationship management services!"));
+            myList.add(new Employer("Jack", "Tech", "Sonos is a developer and manufacturer of audio products best known for its multi-room audio products!"));
+            dao.create(myList);
+            dao.updateId(myList.get(0),10);
+            Assertions.assertDoesNotThrow(() ->  dao.delete(dao.queryForId(10)), "Test passed");
+        }
+
+        //  3) Deleting a collection of employers at once (using the "delete" function of ORMLite) removes all those employers that exist from the table
+        @Test
+        public void testDeleteEmployerCollectionExists() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            myList.add(new Employer("John", "Tech", "An American cloud-based software company focused on customer relationship management services!"));
+            myList.add(new Employer("Jack", "Tech", "Sonos is a developer and manufacturer of audio products best known for its multi-room audio products!"));
+            dao.create(myList);
+            dao.delete(myList);
+            List<Employer> lsRead = dao.queryForAll();
+            assertEquals(0, lsRead.size());
+        }
+        // 4) Deleting a collection of employers at once (using the "delete" function of ORMLite) where none of them exists does not remove any rows from the table
+        @Test
+        public void testDeleteEmployerCollectionNotExist() throws SQLException {
+            List<Employer> myList = new ArrayList<>();
+            dao.delete(myList);
+            List<Employer> lsRead = dao.queryForAll();
+            assertEquals(0, lsRead.size());
+        }
     }
 
 
